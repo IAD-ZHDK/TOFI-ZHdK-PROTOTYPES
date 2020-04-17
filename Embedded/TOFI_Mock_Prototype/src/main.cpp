e8f1e7eaaa51c1ed8e5565d085d649a29f6d47f9
Luke Franzke <lukefranzke@gmail.com>
2020-04-17 09:38:39 +0200

    Moving weighted average added to sensor on hardware side
    This version of the board was given to Jennifer on 17.04.2020

--- Embedded/TOFI_Mock_Prototype/src/main.cpp
+++ Embedded/TOFI_Mock_Prototype/src/main.cpp
@@ -1,13 +1,21 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <BLEPeripheral.h>
-#include <Adafruit_ADS1015.h>
 
 // reference on setting up the nrf52 here: https://docs.platformio.org/en/latest/tutorials/nordicnrf52/arduino_debugging_unit_testing.html#tutorial-nordicnrf52-arduino-debugging-unit-testing
 // reference on using BLEPeripheral here: https://www.hackster.io/gov/imu-to-you-ae53e1#code
 
 /// ********************************** Hardware ******************************************************
-Adafruit_ADS1115 ads1115(0x49);
+const int minDifference = 5; //minimum change on adc before value is sent
+u_int16_t adc0Last = 0;
+u_int16_t adc1Last = 0;
+u_int16_t adc2Last = 0;
+u_int16_t adc3Last = 0;
+// weighted moving avarage values
+u_int16_t adc0 = 0;
+u_int16_t adc1 = 0;
+u_int16_t adc2 = 0;
+u_int16_t adc3 = 0;
 // ********************************** BLE ******************************************************
 const char *localName = "TOFI_MOCK_TRAINER";
 BLEPeripheral TOFIPeripheral = BLEPeripheral();
@@ -18,46 +26,42 @@ BLEService TOFIService = BLEService("ff9c1e42-7b32-11ea-bc55-0242ac130003");
 //BLECharCharacteristic LEDCharacteristic = BLECharCharacteristic("FFD1", BLERead | BLEWrite);
 //BLEDescriptor LEDDescriptor = BLEDescriptor("2901", "LED");
 
-BLECharacteristic sensorCharacteristic("ff9c1e42-7b32-11ea-bc55-0242ac130003", BLERead | BLENotify, 6);
+BLECharacteristic sensorCharacteristic("ff9c1e42-7b32-11ea-bc55-0242ac130003", BLERead | BLENotify, 8);
 BLEDescriptor sensorDescriptor("2901", "Sensors");
 
 //BLECharacteristic btnCharacteristic("ff9c1e42-7b32-11ea-bc55-0242ac130003", BLERead | BLENotify, 1);
 //BLEDescriptor btnDescriptor("2901", "btns");
-
-
-
-
 
 //****************************************************************************************************
 
 void setupBLE();
 
 boolean connected = false;
-int lastMillis;
+double lastMillisLed;
+double lastMillisPoll;
 boolean oscilator = false;
 /**
 * The union directive allows 3 variables to share the same memory location.
 */
 union {
-  uint16_t a[3];
-  unsigned char bytes[6];
+  uint16_t a[4];
+  unsigned char bytes[8];
 } sensorData;
 
 void setup()
 {
-  // ADC
-  ads1115.begin();  // Initialize ads1115
   //
   pinMode(PIN_LED1, OUTPUT);
   digitalWrite(PIN_LED1, HIGH);
   pinMode(PIN_LED2, OUTPUT);
   digitalWrite(PIN_LED2, HIGH);
   setupBLE();
+  analogReadResolution(14);
   pinMode(PIN_A0, INPUT);
-  //pinMode(PIN_BUTTON1, INPUT);
-  //pinMode(PIN_BUTTON2, INPUT);
-  //pinMode(PIN_BUTTON3, INPUT);
-  //pinMode(PIN_BUTTON4, INPUT);
+  pinMode(PIN_A1, INPUT);
+  pinMode(PIN_A2, INPUT);
+  pinMode(PIN_A3, INPUT);
+  pinMode(PIN_BUTTON1, INPUT);
 }
 
 void loop()
@@ -67,42 +71,50 @@ void loop()
   {
     while (central.connected())
     {
-      if (connected)
+
+      double time = millis();
+      // led blink while connected
+      if (time - lastMillisLed > 600)
       {
-        // led blink while connected
-        int time = millis();
-        if (time-lastMillis>600) {
-        lastMillis = time;
+        lastMillisLed = time;
         oscilator = !oscilator;
         digitalWrite(PIN_LED1, oscilator);
         digitalWrite(PIN_LED2, !oscilator);
+      }
+
+      if (time - lastMillisPoll > 5)
+      {
+        lastMillisPoll = time;
+        adc0 = floor(adc0 * 0.6);
+        adc1 = floor(adc1 * 0.6);
+        adc2 = floor(adc2 * 0.6);
+        adc3 = floor(adc3 * 0.6);
+
+        //impedence is too high for analog pins, so double reading here as a workaround to avoid wrong measurements 
+        analogRead(PIN_A0);
+        adc0 = floor(analogRead(PIN_A0) * 0.4);
+        analogRead(PIN_A1);
+        adc1 = floor(analogRead(PIN_A1) * 0.4);
+        analogRead(PIN_A2);
+        adc2 = floor(analogRead(PIN_A2) * 0.4);
+        analogRead(PIN_A3);
+        adc3 = floor(analogRead(PIN_A3) * 0.4);
+        //sensor data
+
+        if (abs(adc0Last - adc0) > minDifference || abs(adc1Last - adc1) > minDifference || abs(adc2Last - adc2) > minDifference || abs(adc3Last - adc3) > minDifference)
+        {
+          adc0Last = adc0;
+          adc1Last = adc1;
+          adc2Last = adc2;
+          adc3Last = adc3;
+          sensorData.a[0] = adc0;
+          sensorData.a[1] = adc1;
+          sensorData.a[2] = adc2;
+          sensorData.a[3] = adc3;
+          unsigned char *sensors = (unsigned char *)&sensorData;
+          sensorCharacteristic.setValue(sensors, 8);
         }
       }
-      int adc0 = ads1115.readADC_SingleEnded(0);
-      int adc1 = ads1115.readADC_SingleEnded(1);
-      int adc2 = ads1115.readADC_SingleEnded(2);
-      //sensor data
-      sensorData.a[0] = adc0;
-      sensorData.a[1] = adc1;
-      sensorData.a[2] = adc2;
-      unsigned char *sensors = (unsigned char *)&sensorData;
-      sensorCharacteristic.setValue(sensors, 6);
-      //btn data
-      /*
-      boolean btn1 = !digitalRead(PIN_BUTTON1); 
-      boolean btn2 = !digitalRead(PIN_BUTTON2); 
-      boolean btn3 = !digitalRead(PIN_BUTTON3); 
-      boolean btn4 = !digitalRead(PIN_BUTTON4);  
-      uint8_t Buffer= 00000000 ^ btn1;
-      Buffer = Buffer << 1;
-      Buffer= Buffer ^ btn2;
-      Buffer = Buffer << 1;
-      Buffer= Buffer ^ btn3;
-      Buffer = Buffer << 1;
-      Buffer= Buffer ^ btn4;
-      unsigned char *btns = (unsigned char *)&Buffer;
-      btnCharacteristic.setValue(btns, 1);
-      */
     }
   }
 }
@@ -144,9 +156,9 @@ void setupBLE()
   TOFIPeripheral.setEventHandler(BLEConnected, blePeripheralConnectHandler);
   TOFIPeripheral.setEventHandler(BLEDisconnected, blePeripheralDisconnectHandler);
 
-  const unsigned char initializerSensor[6] = {0, 0, 0, 0, 0, 0};
-  sensorCharacteristic.setValue(initializerSensor, 6);
-  
+  const unsigned char initializerSensor[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+  sensorCharacteristic.setValue(initializerSensor, 8);
+
   //const unsigned char initializerbtn[1] = {0};
   //btnCharacteristic.setValue(initializerbtn, 1);
 
--- Web/Demo_Game/src/app.js
+++ Web/Demo_Game/src/app.js
@@ -17,9 +17,10 @@ const sketch = (p) => {
   }
 
   p.setup = function () {
-    sensorValues[0] = 65534
-    sensorValues[1] = 65534
-    sensorValues[2] = 65534
+    sensorValues[0] = 16384
+    sensorValues[1] = 16384
+    sensorValues[2] = 16384
+    sensorValues[3] = 16384
     p.createCanvas(p.windowWidth, p.windowHeight)
     p.background(0)
     myBLE = new P5ble()
@@ -37,12 +38,12 @@ const sketch = (p) => {
       p.background(0)
       // p.normalMaterial()
       let spacing = p.windowWidth / sensorValues.length
+      p.translate((spacing / 2), p.windowHeight / 2)
       for (let i = 0; i < sensorValues.length; i++) {
         p.push()
-        p.translate((p.windowWidth / 2) - spacing, p.windowHeight / 2)
         p.translate(spacing * i, 0)
         p.fill(255)
-        let radius = p.map(sensorValues[i], 0, 65534, 10, spacing / 3)
+        let radius = p.map(sensorValues[i], 0, 16384, 10, spacing * 0.8)
         p.ellipse(0, 0, radius, radius)
         p.text(sensorValues[i], 0, spacing / 3 * 1.20)
         p.pop()
@@ -65,14 +66,6 @@ const sketch = (p) => {
 function connectAndStartNotify () {
   // Connect to a device by passing the service UUID
   myBLE.connect(serviceUuid, gotCharacteristics)
-  // You can also filter devices by name
-  // myBLE.connect({
-  //   filters: [{
-  //     services: [serviceUuid],
-  //   }, {
-  //     name: 'ArduinoIMU'
-  //   }]
-  // }, gotCharacteristics)
 }
 
 // A function that will be called once got characteristics
@@ -89,11 +82,10 @@ function gotCharacteristics (error, characteristics) {
     if (i === 0) {
       sensorCharacteristic = characteristics[i]
       // Set datatype to 'custom', p5.ble.js won't parse the data, will return data as it is.
+
       myBLE.startNotifications(sensorCharacteristic, handleSensor, 'custom')
       console.log('characteristics: 1')
     } else if (i === 1) {
-      sensorCharacteristic = characteristics[i]
-      // myBLE.startNotifications(sensorCharacteristic, handlebyte, 'custom')
       console.log('characteristics: 2')
     } else if (i === 2) {
       console.log('characteristics: 3')
@@ -104,47 +96,20 @@ function gotCharacteristics (error, characteristics) {
 }
 
 function onDisconnected () {
-  console.log('Device got disconnected.')
+  console.log('Device was disconnected.')
   isConnected = false
 }
 
-// A function that will be called once got characteristics
-/*
-function handlebyte (data) {
-  let byteArray = [0, 0, 0, 0]
-  let btnData = (data.getUint8(0, true)).toString(2)
-  for (let i = 0; i < byteArray.length; i++) {
-    byteArray[i] = btnData & 1
-    console.log(i + '_' + byteArray[i])
-    btnData = btnData >> 1
-  }
-  console.log('0')
-  if (byteArray[0] === 1) {
-    // console.log(byteArray[0])
-    color = '#FFF3D9'
-  }
-  if (byteArray[1] === 1) {
-    // console.log(byteArray[1])
-    color = '#FFE6B3'
-  }
-  if (byteArray[2] === 1) {
-    // console.log(byteArray[2])
-    color = '#47AAD9'
-  }
-  if (byteArray[3] === 1) {
-    // console.log(byteArray[3])
-    color = '#2E6E8D'
-  }
-}
-*/
 function handleSensor (data) {
   // weighted moving average of values
   sensorValues[0] = Math.floor(sensorValues[0] * 0.8)
   sensorValues[1] = Math.floor(sensorValues[1] * 0.8)
   sensorValues[2] = Math.floor(sensorValues[2] * 0.8)
+  sensorValues[3] = Math.floor(sensorValues[3] * 0.8)
   sensorValues[0] += Math.floor(data.getUint16(0, true) * 0.2)
   sensorValues[1] += Math.floor(data.getUint16(2, true) * 0.2)
   sensorValues[2] += Math.floor(data.getUint16(4, true) * 0.2)
+  sensorValues[3] += Math.floor(data.getUint16(6, true) * 0.2)
 }
 
 let PFIVE = new P5(sketch, containerElement)
